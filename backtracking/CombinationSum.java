bc3d79996458792d69a9e2e916cfdc4cd029d2a7
Ajay Gaur <daiict218@gmail.com>
2018-10-21 18:30:58 +0530

    Update CombinationSum.java
    Removes unnecessary rercursion

--- backtracking/CombinationSum.java
+++ backtracking/CombinationSum.java
@@ -1,55 +1,58 @@
 public class Solution {
-    public ArrayList<ArrayList<Integer>> solution = new ArrayList<ArrayList<Integer>>();
-    public ArrayList<ArrayList<Integer>> combinationSum(ArrayList<Integer> A, int B) {
+  public ArrayList<ArrayList<Integer>> solution = new ArrayList<ArrayList<Integer>>();
+  public ArrayList<ArrayList<Integer>> combinationSum(ArrayList<Integer> A, int B) {
         Collections.sort(A);
-        ArrayList<Integer> list = removeDuplicates(A);
-        
+        ArrayList<Integer> l = removeDuplicates(A);
         ArrayList<Integer> subset = new ArrayList<Integer>();
-        combinationSumUtil(list, subset, 0, B);
+        combinationSumUtil(l, subset, 0, B);
         return solution;
     }
-    
-    public void combinationSumUtil(ArrayList<Integer> list, ArrayList<Integer> subset, int index, int sum){
-        for(int i=index;i<list.size();i++){
-            subset.add(list.get(i));
-            int sumOfSubset = subsetSum(subset);
-            
-            if(sumOfSubset > sum){
-                combinationSumUtil(list, subset, i + 1, sum);
-            } else if(sumOfSubset < sum){
-                combinationSumUtil(list, subset, i, sum);
-            } else {
-                ArrayList<Integer> newSol = new ArrayList<Integer>(subset);
-                Collections.sort(newSol);
-                solution.add(newSol);
-                combinationSumUtil(list, subset, i+1, sum);
+
+    public void combinationSumUtil(ArrayList<Integer> A, ArrayList<Integer> subset, int index, int B){
+        if(index >= A.size()){
+            return ;
+        }
+        
+        for(int i=index;i<A.size(); i++){
+            subset.add(A.get(i));
+            int s = subsetSum(subset);
+            if(s < B){
+                combinationSumUtil(A, subset, i, B);
+            } 
+            if(s == B){
+                ArrayList<Integer> l = new ArrayList<Integer>(subset);
+                Collections.sort(l);
+                solution.add(l);
             }
-            
             subset.remove(subset.size() - 1);
         }
     }
     
-    public ArrayList<Integer> removeDuplicates(ArrayList<Integer> list){
-        ArrayList<Integer> newList = new ArrayList<Integer>();
+    public ArrayList<Integer> removeDuplicates(ArrayList<Integer> list)
+    {
+        ArrayList<Integer> res = new ArrayList<>(list.size());
         
-        int index = 0;
-        newList.add(list.get(0));
-        for(int i=1;i<list.size();i++){
-            if(newList.get(index) != list.get(i)){
-                newList.add(list.get(i));
-                index++;
+        res.add(list.get(0));
+        
+        int p2 = 0;
+        
+        for(int i = 1 ; i < list.size() ; i++)
+        {
+            if(res.get(p2) != list.get(i))
+            {
+                res.add(list.get(i));
+                p2++;
             }
         }
         
-        return newList;
+        return res;
     }
-    
-    public int subsetSum(ArrayList<Integer> list){
+
+    public int subsetSum(ArrayList<Integer> subset){
         int sum = 0;
-        for(int i=0;i<list.size();i++){
-            sum += list.get(i);
+        for(int i=0;i<subset.size();i++){
+            sum += subset.get(i);
         }
-        
         return sum;
     }
 }
