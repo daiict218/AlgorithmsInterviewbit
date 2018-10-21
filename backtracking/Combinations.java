ce2017b0a111aa4e667c5bd6fcecf9b30cd3ef6e
Ajay Gaur <daiict218@gmail.com>
2018-10-22 01:56:59 +0530

    Indentation

--- backtracking/Combinations.java
+++ backtracking/Combinations.java
@@ -1,49 +1,32 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Collections;
-class main {
-	public static ArrayList<ArrayList<Integer>> solution = new ArrayList<ArrayList<Integer>>();
-	public static ArrayList<ArrayList<Integer>> combine(int n, int k) {
-      ArrayList<Integer> subset = new ArrayList<Integer>();
-      if(n == 0){
-          solution.add(subset);
-          return solution;
-      }
-      ArrayList<Integer> A = new ArrayList<Integer>();
-      for(int i=0;i<n;i++){
-        A.add(i, i+1);
-      }
-
-		// solution.add(subset);
-		combineUtil(A, subset, k, 0);
-		return solution;
-	}
-
-	public static void combineUtil(ArrayList<Integer> A, ArrayList<Integer> subset, int k, int index){
-			if(subset.size() >= A.size()){
-				return;
-			}
-
-			for(int i=index;i<A.size();i++){
-					//including the element
-					subset.add(A.get(i));
-
-					//adding subset to the solution
-          if(subset.size() == k){
-					       solution.add(new ArrayList<Integer>(subset));
-          }
-					combineUtil(A, subset, k, i+1);
-
-					//excluding the element
-					subset.remove(A.get(i));
-			}
-	}
-
-		public static void main(String args[]){
-			Scanner input = new Scanner(System.in);
-			int n = input.nextInt();
-      int k = input.nextInt();
-
-			System.out.println(combine(n, k));
-		}
+public class Solution {
+    ArrayList<ArrayList<Integer>> solution = new ArrayList<ArrayList<Integer>>();
+    public ArrayList<ArrayList<Integer>> combine(int A, int B) {
+        ArrayList<Integer> subset = new ArrayList<Integer>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if(A == 0){
+            solution.add(subset);
+            return solution;
+        }
+        
+        for(int i=0;i<A;i++){
+            list.add(i, i+1);
+        }
+        
+        combineUtil(list, subset, 0, B);
+        return solution;
+    }
+    
+    public void combineUtil(ArrayList<Integer> list, ArrayList<Integer> subset, int index, int k){
+        if(subset.size() >= list.size()){
+            return ;
+        }
+        for(int i=index;i<list.size();i++){
+            subset.add(i, list.get(i));
+            if(subset.size() == k){
+                solution.add(new ArrayList<Integer>(subset));
+            }
+            combineUtil(list, subset, i + 1, k);
+            subset.remove(subset.size() - 1);
+        }
+    }
 }
